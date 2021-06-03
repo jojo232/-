0201c1ba312b5c751df2d247f1bbfba51698fdba
Ubuntu <ubuntu@ip-172-31-1-175.ap-northeast-1.compute.internal>
2021-06-03 05:11:51 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4f5de8016ed9724498bcd7b7c8b00f7510d0c1795e75c20f43e35cb40c012317f08df8fabad9e75283c83b763972133a87b20e311b3a179483541d43a526ebad'
